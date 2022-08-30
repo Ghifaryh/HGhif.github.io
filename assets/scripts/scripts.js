b7acd86a4b1a67176d238d2dac27527284b4e6b2
Ghifaryh <mhghifaryy@gmail.com>
2022-08-30 15:21:12 +0700

    adding switching car animation

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,3 +1,20 @@
 console.log("Cie mau nyari apa hayo");
 console.log("Maap ga pake JS");
 console.log("Anyway, here the prize for checking by: www.bit.do/YeetYeet ");
+
+var localImages = "assets/image/Public Transportation/";
+var imageSources = [
+  localImages + "CaheumCiroyom.png",
+  localImages + "CaheumLedeng.png",
+  localImages + "CaheumRoyom.png",
+  //   localImages + "GedebageSimpang.png",
+];
+
+var index = 0;
+setInterval(function () {
+  if (index === imageSources.length) {
+    index = 0;
+  }
+  document.getElementById("angkot").src = imageSources[index];
+  index++;
+}, 500);
